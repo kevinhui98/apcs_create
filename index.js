8f50c1461c30aca8bc55fe25ab59c663bb874233
kevinhui98 <kevin.hui@envirostudies.org>
2017-04-19 14:22:32 +0000

    pushing everything so far

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-$("#yes").click(function(){
+$("#Yes").click(function(){
     $("#firstMove").show();
-    $("#decision").hide();
+    $("#Decision").hide();
+    $('#evil').append("<img id=eggMan src='http://orig14.deviantart.net/9608/f/2014/303/4/f/the_evil_eggman_by_nibrocrock-d84p1zm.png' />");
 });
-$("#no").click(function(){
-    $("#retry").show();
-    $("#decision").hide();
+$("#No").click(function(){
+    $("#Retry").show();
+    $("#Decision").hide();
 });
 $("#Start").click(function(){
     $("#firstMove").hide();
@@ -14,15 +15,14 @@ $("#Start").click(function(){
 });
 $("#Detour").click(function(){
     $("#firstMove").hide();
-    $("#friends").show();
-    // $('#image').append("<img src= 'http://i.mdldb.net/cache/ejj/v/81y88Nm4_88760f_c.jpg' />");
+    $("#Friends").show();
 });
 $("#Shadow").click(function(){
-    $("#friends").hide();
+    $("#Friends").hide();
     $("#theFight").show();
 });
 $("#Silver").click(function(){
-    $("#friends").hide();
+    $("#Friends").hide();
     $("#theFight").show();
 });
 $('.friend').click(function() {
@@ -31,16 +31,48 @@ $('.friend').click(function() {
         Shadow: 'https://vignette4.wikia.nocookie.net/sonic/images/c/c9/Super_shadow_final.png/revision/latest?cb=20091210224334',
     };
     var friend_name = $(this).attr('id');
-    // console.log( friend_name );
-    // console.log( friends['silver'] );
     $('#image').append("<img id=largeImg src='"+ friends[friend_name] +"' />");
     console.log( friend_name );
 });
 
-$("#spin").click(function(){
+$("#Spin").click(function(){
    $('#image').append("<img id=tornado src='http://25.media.tumblr.com/cf12cb1ce25cc83c0855fcda25f66624/tumblr_mxrgvoLx6o1qe1uqko1_1280.gif' />");
-   document.onkeydown = function(event){
-	if(event.keyCode === 32)	//space
-		player.pressingRight = true;
-}
+    setTimeout(function(){
+        $('#tornado').hide();
+        }, 1000
+    ); 
+    setTimeout(function(){
+        $('#eggMan').hide();
+        }, 1000
+    ); 
+   setTimeout(function(){
+        $('#evil').append("<img id=result src='https://media.giphy.com/media/xUPGcMz3nEbLVVKu1a/giphy.gif' />");
+    setTimeout(function(){
+        alert("You have sent eggman to the moon");
+        }, 3500
+    );
+        }, 1000
+    );
+   setTimeout(function(){
+        $('#lastAction').show();
+        $('#theFight').hide();
+   },1000
+    ); 
+});
+$("#Punch").click(function(){
+    $('#eggMan').hide();
+    $('#evil').append("<img id=result src='https://media.giphy.com/media/xUPGcMz3nEbLVVKu1a/giphy.gif' />");
+    setTimeout(function(){
+        alert("You have sent eggman to the moon");
+        }, 2500
+    );
+    $('#lastAction').show();
+    $('#theFight').hide();
+});
+$("#Retry").click(function(){
+    $("#Redo").hide();
+    $("#Decision").show();
+});
+$('#Restart').click(function(){
+    location.reload();
 });
